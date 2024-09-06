63b1fc2194069b36aa8a7eba99c93c4ad1461e00
Sergivaldo Junior <sergivaldo123@gmail.com>
2024-09-06 17:04:01 -0300

    Abstração do mapeador do modelo 'User' e implementação utilizando o padrão Builder

---
+++ src/main/java/junior/sergivaldo/mytasks/adapter/secondary/jpa/mapper/impl/UserModelMapperBuilderImpl.java
@@ -0,0 +1,30 @@
+package junior.sergivaldo.mytasks.adapter.secondary.jpa.mapper.impl;
+
+import junior.sergivaldo.mytasks.adapter.secondary.jpa.mapper.UserModelMapper;
+import junior.sergivaldo.mytasks.adapter.secondary.jpa.model.UserModel;
+import junior.sergivaldo.mytasks.application.domain.UserEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserModelMapperBuilderImpl implements UserModelMapper {
+
+    @Override
+    public UserModel toModel(UserEntity userEntity) {
+        return UserModel.builder()
+                .id(userEntity.getId())
+                .name(userEntity.getName())
+                .password(userEntity.getPassword())
+                .email(userEntity.getEmail())
+                .build();
+    }
+
+    @Override
+    public UserEntity toEntity(UserModel userModel) {
+        return UserEntity.builder()
+                .id(userModel.getId())
+                .name(userModel.getName())
+                .email(userModel.getEmail())
+                .build();
+    }
+
+}
