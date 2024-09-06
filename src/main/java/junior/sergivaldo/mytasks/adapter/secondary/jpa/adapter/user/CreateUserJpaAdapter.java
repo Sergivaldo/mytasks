1128f0015ee34eb83bf491aa718a0e184f304bb9
Sergivaldo Junior <sergivaldo123@gmail.com>
2024-09-06 17:05:29 -0300

    Implementação do adaptador JPA para criação de um usuário

---
+++ src/main/java/junior/sergivaldo/mytasks/adapter/secondary/jpa/adapter/user/CreateUserJpaAdapter.java
@@ -0,0 +1,29 @@
+package junior.sergivaldo.mytasks.adapter.secondary.jpa.adapter.user;
+
+import junior.sergivaldo.mytasks.adapter.secondary.jpa.mapper.UserModelMapper;
+import junior.sergivaldo.mytasks.adapter.secondary.jpa.repository.UserRepository;
+import junior.sergivaldo.mytasks.application.domain.UserEntity;
+import junior.sergivaldo.mytasks.application.port.out.user.CreateUserAdapter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CreateUserJpaAdapter implements CreateUserAdapter {
+
+    private final UserRepository userRepository;
+
+    private final UserModelMapper userModelMapper;
+
+    @Override
+    public UserEntity create(UserEntity user) {
+        return userModelMapper.toEntity(
+                userRepository.save(
+                        userModelMapper.toModel(
+                                user
+                        )
+                )
+        );
+    }
+
+}
