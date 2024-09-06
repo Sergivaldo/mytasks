0ba616936410dd6fb3017faf4b7f92c0dcfb7002
Sergivaldo Junior <sergivaldo123@gmail.com>
2024-09-06 09:32:11 -0300

    Adicao de adapter para verificar a existencia de uma lista de tarefas pelo nome

--- src/main/java/junior/sergivaldo/mytasks/adapter/primary/api/config/usecase/tasklist/CreateTaskListUseCaseConfig.java
+++ src/main/java/junior/sergivaldo/mytasks/adapter/primary/api/config/usecase/tasklist/CreateTaskListUseCaseConfig.java
@@ -3,13 +3,14 @@ package junior.sergivaldo.mytasks.adapter.primary.api.config.usecase.tasklist;
 import junior.sergivaldo.mytasks.application.port.in.tasklist.CreateTaskListUseCase;
 import junior.sergivaldo.mytasks.application.port.out.tasklist.CreateTaskListAdapter;
 import junior.sergivaldo.mytasks.application.usecases.tasklist.CreateTaskListUseCaseImpl;
+import junior.sergivaldo.mytasks.application.usecases.tasklist.ExistsTaskListByNameAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CreateTaskListUseCaseConfig {
     @Bean
-    public CreateTaskListUseCase createTaskListUseCase(CreateTaskListAdapter createTaskListAdapter) {
-        return new CreateTaskListUseCaseImpl(createTaskListAdapter);
+    public CreateTaskListUseCase createTaskListUseCase(CreateTaskListAdapter createTaskListAdapter, ExistsTaskListByNameAdapter existsTaskListByNameAdapter) {
+        return new CreateTaskListUseCaseImpl(createTaskListAdapter, existsTaskListByNameAdapter);
     }
 }
