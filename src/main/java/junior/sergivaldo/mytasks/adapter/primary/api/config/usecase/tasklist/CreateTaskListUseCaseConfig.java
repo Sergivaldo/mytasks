65183e54c44c79d86d00bb06c092268417151b37
Sergivaldo Junior <sergivaldo123@gmail.com>
2024-09-06 14:37:40 -0300

    Adição da dependencia FindBoardByIdUseCase ao caso de uso de criação da lista de tarefas

--- src/main/java/junior/sergivaldo/mytasks/adapter/primary/api/config/usecase/tasklist/CreateTaskListUseCaseConfig.java
+++ src/main/java/junior/sergivaldo/mytasks/adapter/primary/api/config/usecase/tasklist/CreateTaskListUseCaseConfig.java
@@ -1,16 +1,17 @@
 package junior.sergivaldo.mytasks.adapter.primary.api.config.usecase.tasklist;
 
+import junior.sergivaldo.mytasks.application.port.in.board.FindBoardByIdUseCase;
 import junior.sergivaldo.mytasks.application.port.in.tasklist.CreateTaskListUseCase;
 import junior.sergivaldo.mytasks.application.port.out.tasklist.CreateTaskListAdapter;
+import junior.sergivaldo.mytasks.application.port.out.tasklist.ExistsTaskListByNameAdapter;
 import junior.sergivaldo.mytasks.application.usecases.tasklist.CreateTaskListUseCaseImpl;
-import junior.sergivaldo.mytasks.application.usecases.tasklist.ExistsTaskListByNameAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CreateTaskListUseCaseConfig {
     @Bean
-    public CreateTaskListUseCase createTaskListUseCase(CreateTaskListAdapter createTaskListAdapter, ExistsTaskListByNameAdapter existsTaskListByNameAdapter) {
-        return new CreateTaskListUseCaseImpl(createTaskListAdapter, existsTaskListByNameAdapter);
+    public CreateTaskListUseCase createTaskListUseCase(CreateTaskListAdapter createTaskListAdapter, ExistsTaskListByNameAdapter existsTaskListByNameAdapter, FindBoardByIdUseCase findBoardByIdUseCase) {
+        return new CreateTaskListUseCaseImpl(createTaskListAdapter, existsTaskListByNameAdapter, findBoardByIdUseCase);
     }
 }
